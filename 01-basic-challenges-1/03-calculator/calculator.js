a58e516e95beeb910a252c99ca84dbe4fd171008
Parikhit <parikhitritzz@gmail.com>
2024-01-19 23:41:59 +0530

    Completed 03 from  Basic-challenges-1

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,24 @@
-function calculator() {}
+function calculator(num1, num2, operation) {
+    let result;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            throw new Error('Invalid Operation');
+    }
+
+    return result;
+}
 
 module.exports = calculator;

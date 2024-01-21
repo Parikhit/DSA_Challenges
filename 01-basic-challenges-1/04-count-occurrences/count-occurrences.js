7399db6f3210093461a15a345dc82ec04241444e
Parikhit <parikhitritzz@gmail.com>
2024-01-21 13:02:03 +0530

    Done 01-BasicChallenges1 --> 04

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,21 @@
-function countOccurrences() {}
+/* Way 1
+function countOccurrences(str, char) {
+    let count = 0;
+
+    for (let i = 0; i <= str.length; i++) {
+        if (str[i] === char) {
+            count++;
+        }
+    }
+
+    return count;
+}
+*/
+
+// Way 2
+
+const countOccurrences = (str, char) => {
+    return str.split(char).length - 1;
+};
 
 module.exports = countOccurrences;

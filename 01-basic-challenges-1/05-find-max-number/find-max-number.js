5a22790167d24e47b693cd38542d94071b66ad11
Parikhit <parikhitritzz@gmail.com>
2024-01-21 20:18:44 +0530

    Done 01-BasicChallenges1 --> 05

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,29 @@
-function findMaxNumber() {}
+//1st way:
+/*
+function findMaxNumber(arr) {
+    return Math.max(...arr);
+}
+*/
+
+//2nd Way
+/*
+function findMaxNumber(arr) {
+    const sortedArr = arr.sort((a, b) => a - b);
+    return sortedArr[sortedArr.length - 1];
+}
+*/
+
+//3rd Way
+function findMaxNumber(arr) {
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
 
 module.exports = findMaxNumber;
